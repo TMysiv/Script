ec98dd7a4174cf0a473b267585f1a3b4ab4c5e61
TMysiv <tarasmysiv@gmail.com>
2021-11-16 21:39:20 +0200

    add additional

--- Task_3/Additional/main.js
+++ Task_3/Additional/main.js
@@ -1,13 +1,13 @@
 // 1. Створити пустий масив та :
 
 // a. заповнити його 50 парними числами за допомоги циклу.
-// let array = [];
+// let arr = [];
 // let x = 2;
 // for (let i=0; i<50;i++){
-//     array[i]=x;
+//     arr[i]=x;
 //     x=x+2;
 // }
-// console.log(array)
+// console.log(arr)
 
 //     b. заповнити його 50 непарними числами за допомоги циклу.
 // let array = [];
@@ -37,24 +37,26 @@
 // 2. Вивести за допомогою console.log кожен третій елемен
 
 // let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-// for (let i=2;i<array.length; i+=2){
+// for (let i=2;i<array.length; i+=3){
 //         console.log(array[i]);
 // }
 
-// // 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
-// for (let i=2;i<array.length;i+=2){
+// 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
+// let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// for (let i=2;i<array.length;i+=3){
 //     if (array[i]%2===0) {
 //         console.log(array[i]);
 //     }
 // }
 
 // 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
-// for (let i=2;i<array.length;i+=2){
+// let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// let element = [];
+// for (let i=2;i<array.length;i+=3){
 //     if (array[i]%2===0) {
-//         let element = array[i];
-//         console.log(element);
+//          element[i] = array[i];
 //     }
-// }
+// }console.log(element);
 
 // 5. Вивести кожен елемент масиву, сусід справа якого є парним EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
 
@@ -77,11 +79,12 @@
 // 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
 
 // let array = [];
+// let element = [];
 // for ( let i=0; i<10; i++){
 //     array[i] = Math.floor(Math.random()*20);
-//     let  element= array[i]*5;
-//     console.log(element);
+//     element[i]= array[i]*5;
 // }
+// console.log(element);
 
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
 
@@ -93,33 +96,6 @@
 //     }
 // }
 // console.log(newarray);
-
-// - Дано 2 масиви з рівною кількістю об'єктів.З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
-// Записати цей об'єкт в новий масив
-// Масиви:
-
-
-let usersWithId = [
-    {id: 1, name: 'vasya', age: 31, status: false},
-    {id: 2, name: 'petya', age: 30, status: true},
-    {id: 3, name: 'kolya', age: 29, status: true},
-    {id: 4, name: 'olya', age: 28, status: false}
-];
-
-let citiesWithId = [
-    {user_id: 3, country: 'USA', city: 'Portland'},
-    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-    {user_id: 2, country: 'Poland', city: 'Krakow'},
-    {user_id: 4, country: 'USA', city: 'Miami'}
-];
-
-for (let i=0;i<4;i++){
-    if (usersWithId[i].id===citiesWithId[i].user_id){
-        console.log(`${usersWithId[i].name} ${citiesWithId[i].city}`)
-    }
-}
-
-
 
 
 // - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
@@ -139,7 +115,29 @@ for (let i=0;i<4;i++){
 //     }
 // console.log(newarray)
 
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
 
+// let array = [ 'a', 'b', 'c'];
+// let slovo = '';
+// for (let i = 0; i<array.length;i++){
+//     slovo += array[i];
+// }
+// console.log(slovo)
 
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+// let array = [ 'a', 'b', 'c'];
+// let slovo = '';
+// let i= 0;
+// while (i<array.length){
+//     slovo +=array[i];
+//     i++
+// }
+// console.log(slovo)
 
-
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+// let array = [ 'a', 'b', 'c'];
+// let slovo = '';
+// for (let i of array){
+//     slovo +=i;
+// }
+// console.log(slovo)
